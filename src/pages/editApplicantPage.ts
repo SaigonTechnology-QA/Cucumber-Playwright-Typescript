fad3bd7ee6d8a4cb545796e747abf6099ebcc3a2
baonguyenmhSTS <bao.nguyenmh@saigontechnology.com>
2023-04-04 09:29:34 +0700

    Create editApplicantPage.ts

---
+++ src/pages/editApplicantPage.ts
@@ -0,0 +1,106 @@
+import { CommonPage } from './commonPage';
+import { ICustomWorld } from '../support/custom-world';
+import { DataUtils } from '../utils/dataUtils';
+import { Locator, Page } from '@playwright/test';
+
+export class EditApplicantPage {
+  readonly page: Page;
+  readonly txtSearchApplicant: Locator;
+  readonly btnShowActionBurger: Locator;
+  readonly btnEditApplicant: Locator;
+  readonly txtFirstName: Locator;
+  readonly txtLastName: Locator;
+  readonly txtPrimaryEmail: Locator;
+  readonly txtPrimaryPhoneNumber: Locator;
+  readonly btnEditSkillIcon: Locator;
+  readonly btnSaveSkill: Locator;
+  readonly updateSuccessToast: Locator;
+  commonPage: CommonPage;
+  constructor(page: Page, iCustomWorld: ICustomWorld) {
+    this.page = page;
+    this.txtSearchApplicant = page.locator(
+      '//input[@placeholder="Name, email, linkedIn, github, facebook"]',
+    );
+    this.btnShowActionBurger = page.locator('//button[@title="Click to show actions"]');
+    this.btnEditApplicant = page.locator('//button/span[contains(text(),"Edit applicant")]');
+    this.txtFirstName = page.locator('input[name="firstName"]');
+    this.txtLastName = page.locator('input[name="lastName"]');
+    this.txtPrimaryEmail = page.locator('input[formcontrolname="primaryEmail"]');
+    this.txtPrimaryPhoneNumber = page.locator('input[formcontrolname="primaryPhoneNumber"]');
+    this.btnEditSkillIcon = page.locator('//button[@title="Edit"]');
+    this.btnSaveSkill = page.locator(
+      '//div[@class="col-12 p-0 footer"]//child::button[starts-with(normalize-space(text()),"Save")]',
+    );
+    this.updateSuccessToast = this.page.getByRole('alertdialog', { name: 'Updated candidate' });
+    this.commonPage = new CommonPage(page, iCustomWorld);
+  }
+
+  async searchApplicantWithEmail(email: string) {
+    await this.commonPage.clearTextField(this.txtSearchApplicant);
+    await this.txtSearchApplicant.fill(email);
+    await this.page.waitForEvent('response');
+    await this.page.waitForLoadState();
+    await this.page.waitForLoadState('domcontentloaded');
+    await this.page.waitForSelector('//table/tbody/tr/td[3]');
+  }
+
+  async clickOnShowActionBurgerButton() {
+    await this.btnShowActionBurger.click();
+  }
+
+  async clickOnEditApplicantOption(email: string) {
+    const emailSearch =
+      process.env.candidateApplicantEmail !== undefined
+        ? process.env.candidateApplicantEmail
+        : email;
+    await this.searchApplicantWithEmail(emailSearch);
+    await this.clickOnShowActionBurgerButton();
+    await this.btnEditApplicant.click();
+  }
+
+  async updateApplicantWithRequireField() {
+    const dataUtils = new DataUtils();
+    const applicantData = dataUtils.getCandidateDataByType('required').recruitmentInfo;
+    const applicantCandidateData = dataUtils.getCandidateDataByType('required').basicInfo;
+
+    //clear & update First Name
+    await this.commonPage.clearTextField(this.txtFirstName);
+    await this.txtFirstName.fill(applicantCandidateData.firstName + ' updated');
+
+    //clear & update Last Name
+    await this.commonPage.clearTextField(this.txtLastName);
+    await this.txtLastName.fill(applicantCandidateData.lastName + ' updated');
+
+    //clear & update Email
+    await this.commonPage.clearTextField(this.txtPrimaryEmail);
+    process.env.candidateEmailUpdate = await dataUtils.getEmail();
+    await this.txtPrimaryEmail.fill(process.env.candidateEmailUpdate);
+
+    //clear & update Phone Number
+    await this.commonPage.clearTextField(this.txtPrimaryPhoneNumber);
+    process.env.candidatePhoneUpdate = await dataUtils.getPhoneNumber();
+    await this.txtPrimaryPhoneNumber.fill(process.env.candidatePhoneUpdate);
+
+    //Update Location
+    await this.commonPage.selectDropdownWithLastFoundValue(
+      'Location',
+      applicantData.workingLocation,
+    );
+
+    //Update Skill
+    await this.updateSkillOnApplicant(applicantData.skills[0]);
+  }
+
+  async updateSkillOnApplicant(newSkill: string) {
+    //Click on Edit icon
+    await this.btnEditSkillIcon.click();
+    //Select new skill
+    await this.commonPage.selectDropdownWithLastFoundValue('Skill Name', newSkill);
+    //Click on Save skill CTA
+    await this.btnSaveSkill.click();
+  }
+
+  async getToastMessageLocator(): Promise<Locator> {
+    return this.updateSuccessToast;
+  }
+}

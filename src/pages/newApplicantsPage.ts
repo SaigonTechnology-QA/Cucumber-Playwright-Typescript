00378a7fc495fa322d2dafbd16e670bd9a98f23f
baonguyenmhSTS <bao.nguyenmh@saigontechnology.com>
2023-04-05 13:57:00 +0700

    Update newApplicantsPage.ts

--- src/pages/newApplicantsPage.ts
+++ src/pages/newApplicantsPage.ts
@@ -5,20 +5,32 @@ import { Locator, Page } from '@playwright/test';
 
 export class NewApplicants {
   readonly page: Page;
-  readonly btnAddNewCandicate: Locator;
+  readonly btnAddNewCandidate: Locator;
+  readonly txtSearchKey: Locator;
   commonPage: CommonPage;
   constructor(page: Page, iCustomWorld: ICustomWorld) {
     this.page = page;
     this.commonPage = new CommonPage(page, iCustomWorld);
-    this.btnAddNewCandicate = page.getByRole('link', { name: 'Add New Candidate' });
+    this.btnAddNewCandidate = page.getByRole('link', { name: 'Add New Candidate' });
+    this.txtSearchKey = page.locator(
+      '//input[@placeholder="Name, email, linkedIn, github, facebook"]',
+    );
   }
   async goto() {
+    await this.page.goto(config.BASE_URL + 'new-applicants');
     await this.page.waitForLoadState('networkidle');
     await this.page.waitForLoadState();
     await this.page.waitForLoadState('domcontentloaded');
-    await this.page.goto(config.BASE_URL + 'new-applicants');
   }
   async goToNewCandidatePage() {
-    await this.btnAddNewCandicate.click();
+    await this.btnAddNewCandidate.click();
+  }
+  async searchNewApplicant(email: string) {
+    await this.commonPage.clearTextField(this.txtSearchKey);
+    await this.txtSearchKey.fill(email);
+    await this.page.waitForEvent('response');
+    await this.page.waitForLoadState();
+    await this.page.waitForLoadState('domcontentloaded');
+    await this.page.waitForSelector('//table/tbody/tr/td[3]');
   }
 }

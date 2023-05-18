20430dc01ebc15f3d960d7f5db4ab2fc6dce5022
baonguyenmhSTS <bao.nguyenmh@saigontechnology.com>
2023-05-18 08:49:25 +0700

    Update editApplicant.steps.ts

--- src/steps/editApplicant.steps.ts
+++ src/steps/editApplicant.steps.ts
@@ -1,17 +1,19 @@
 import { ICustomWorld } from '../support/custom-world';
 import { EditApplicantPage } from '../pages/editApplicantPage';
 import { NewApplicants } from '../pages/newApplicantsPage';
+import { CommonPage } from '../pages/commonPage';
 import { When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
 
 When(
   'I click on "Edit Applicant" option in the hamburger menu button',
   async function (this: ICustomWorld) {
     const page = this.page!;
+    const newApplicantPage = new NewApplicants(page, this);
+    const commonPage = new CommonPage(page, this);
     const emailApplicant =
       process.env.candidateApplicantEmail !== undefined ? process.env.candidateApplicantEmail : '';
-    const editApplicantPage = new EditApplicantPage(page, this);
-    await editApplicantPage.clickOnEditApplicantOption(emailApplicant);
+    await newApplicantPage.searchNewApplicant(emailApplicant);
+    await commonPage.clickOnOptionOnContextBurgerMenu('Edit applicant');
   },
 );
 
@@ -26,18 +28,19 @@ When(
 
 Then('the "Updated candidate" message should be displayed', async function (this: ICustomWorld) {
   const page = this.page!;
-  const editApplicantPage = new EditApplicantPage(page, this);
-  expect((await editApplicantPage.getToastMessageLocator()).isVisible);
+  const commonPage = new CommonPage(page, this);
+  await commonPage.checkToastMessageContent('Updated candidate');
 });
 
 Then('the newly updated Applicant should be saved', async function (this: ICustomWorld) {
   //Go to Applicants page
   const page = this.page!;
   const newApplicants = new NewApplicants(page, this);
+  const commonPage = new CommonPage(page, this);
   await newApplicants.goto();
   //Search with updated email
-  const email =
+  const emailApplicant =
     process.env.candidateEmailUpdate !== undefined ? process.env.candidateEmailUpdate : '';
-  await newApplicants.searchNewApplicant(email);
-  await expect(page.getByTitle(email)).toHaveCount(1);
+  await newApplicants.searchNewApplicant(emailApplicant);
+  await commonPage.checkEmailApplicantAfterSearch(emailApplicant, 1);
 });

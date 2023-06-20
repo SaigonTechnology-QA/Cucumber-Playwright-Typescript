9752cd0f6033ee09f0e2dee1cb3488bef4be769a
baonguyenmhSTS <bao.nguyenmh@saigontechnology.com>
2023-06-20 08:41:24 +0700

    Update completeProfile.steps.ts

--- src/steps/completeProfile.steps.ts
+++ src/steps/completeProfile.steps.ts
@@ -9,9 +9,6 @@ When(
   async function (this: ICustomWorld) {
     const page = this.page!;
     const newApplicants = new NewApplicants(page, this);
-    const emailSearch =
-      process.env.candidateApplicantEmail !== undefined ? process.env.candidateApplicantEmail : '';
-    await newApplicants.searchNewApplicant(emailSearch);
     await newApplicants.clickHamburgerButton();
     await newApplicants.clickCompleteProfileMenuItem();
   },
@@ -22,7 +19,7 @@ When(
   async function (this: ICustomWorld) {
     const page = this.page!;
     const completeProfile = new CompleteProfile(page, this);
-    await completeProfile.selectDataToCompleteACandidateProfile;
+    await completeProfile.selectDataToCompleteACandidateProfile();
   },
 );
 
@@ -35,6 +32,16 @@ When(
   },
 );
 
+When(
+  `I search for existing a new Applicant without a phone number`,
+  async function (this: ICustomWorld) {
+    const existingApplicantWithoutPhone = "hieunguyen22364@gmail.com";
+    const page = this.page!;
+    const newApplicants = new NewApplicants(page, this);
+    await newApplicants.searchNewApplicant(existingApplicantWithoutPhone);
+  },
+);
+
 Then(
   `The "Candidates have CVs have been moved to Interview Process." message should be displayed`,
   async function (this: ICustomWorld) {
@@ -42,6 +49,17 @@ Then(
     const commonPage = new CommonPage(page, this);
     await commonPage.checkToastMessageContent(
       'Candidates have CVs have been moved to Interview Process.  Candidates have no CVs have been moved to Candidate Pool.',
+    );
+  },
+);
+
+Then(
+  `The "Candidate profile is missing email or phone so cannot mark this profile as completed." error message should be displayed`,
+  async function (this: ICustomWorld) {
+    const page = this.page!;
+    const commonPage = new CommonPage(page, this);
+    await commonPage.checkToastMessageContent(
+      'Candidate profile is missing email or phone so cannot mark this profile as completed.',
     );
   },
 );
